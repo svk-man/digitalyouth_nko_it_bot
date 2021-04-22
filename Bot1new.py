f35c96536f882fd155c8fa95e3d4cf32991cf6f7
Artrazr <82875321+Artrazr@users.noreply.github.com>
2021-04-22 19:53:15 +0300

    Add files via upload

---
+++ Bot1new.py
@@ -0,0 +1,59 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup,\
+    KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+bot = Bot(token = '1756077502:AAEW_cavIZErG8g6pCK_axKOafPtxNB--8s')
+dp = Dispatcher(bot)
+    
+@dp.message_handler (commands = ['help'])
+async def process_start_command(message: types.Message):
+    await message.reply('Здесь выводится информация о боте и подсказки')
+
+
+#попытка сделать кнопку регистрации
+but_reg = KeyboardButton('/регистрация')
+regis = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard = True).add(but_reg)
+
+@dp.message_handler(commands = ['start'])
+async def new_command(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Привет!  \nТы ещё не зарегистрирован, прошу пройти небольшую регистрацию :)',
+                           reply_markup = regis)
+    
+#Кнопки
+
+inline_btn_1 = InlineKeyboardButton('Кнопка №1', callback_data='button1')
+inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
+
+
+#Это пул кнопок
+inline_but_pool = InlineKeyboardMarkup()
+
+inline_bt1 = InlineKeyboardButton('Представитель IT', callback_data='btn1')
+inline_bt2 = InlineKeyboardButton('Представитель НКО', callback_data='btn2')
+inline_bt3 = InlineKeyboardButton('Модератор', callback_data='btn3')
+inline_but_pool.add(inline_bt1, inline_bt2, inline_bt3)
+
+
+#Попытка сделать развилку как планировали
+@dp.callback_query_handler(lambda c1: c1.data and c1.data.startswith('btn'))
+async def call_but_pool(callback_query: types.CallbackQuery):
+    code = callback_query.data[-1]    
+    if code.isdigit():
+        code = int(code)
+    if code == 1:        
+        await bot.send_message(callback_query.from_user.id, 'Введите ваше Имя')        
+    if code == 2:
+        await bot.send_message(callback_query.from_user.id, 'Введите наименование НКО')
+    if code == 3:
+        await bot.send_message(callback_query.from_user.id, 'Введите Ваш ключ для регистрации')
+@dp.message_handler(commands = ['регистрация'])
+async def proc_com(message: types.Message):
+    await message.reply('В качестве кого Вы хотите зарегистрироваться?', reply_markup = inline_but_pool)
+
+    
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates = True)
+    
